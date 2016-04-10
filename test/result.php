3bd55247c493c57b6f389e424f4e6f26bc288d51
iGilga <pyua87@gmail.com>
2016-04-10 13:45:10 +0300

    добавлена обработка резултата

--- test/result.php
+++ test/result.php
@@ -1,7 +1,18 @@
+<?
+$result=0;
+if(isset($_SESSION['test'])){
+	$answer=parse_ini_file("answers.ini");
+	foreach ($_SESSION['test'] as $value) {
+		if(array_key_exists($value,$answer))
+		$result+=(int)$answer[$value];
+	}
+	session_destroy();
+}
+?>
 <table width="100%">
 	<tr>
 		<td align="left">
-		
+			<p>Your result:<?=$result?> in 30</p>
 		</td>
 	</tr>
 </table>
