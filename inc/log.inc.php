a3d9723df8c708648f0b16743c56da9778aea15e
Alex Lich <psixwill@gmail.com>
2016-04-16 15:43:56 +0300

    Извлекает время, начальный урл, урл перехода, затем их записать в файл php.log

--- inc/log.inc.php
+++ inc/log.inc.php
@@ -1,1 +1,6 @@
 <?
+$dt = date('m-d-Y h:i:s');
+$page = $_SERVER['REQUEST_URI'];
+$ref = $_SERVER['HTTP_REFERER'];
+$path="$dt :  $page --> $ref</br>\r\n";
+file_put_contents("path.log", "$path", FILE_APPEND);
