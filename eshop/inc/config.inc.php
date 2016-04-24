9ecad181aa4b983025f00d55b26a852b728e5672
Alex Lich <psixwill@gmail.com>
2016-04-24 19:41:43 +0300

    Объявление констант, переменных и установка соединения с сервером базы данных

--- eshop/inc/config.inc.php
+++ eshop/inc/config.inc.php
@@ -1,1 +1,26 @@
 <?php
+
+/*define('DB_HOST', 'localhost');
+define('DB_LOGIN', 'root');
+define('DB_PASSWORD', '12345');
+define('DB_NAME', 'eshop');
+define('ORDERS_LOG','orders.log');*/
+$DB_HOST="localhost";
+$DB_LOGIN="root";
+$DB_PASSWORD="12345";
+$DB_NAME="eshop";
+$baske=array();
+$count=0;
+
+$link = mysqli_connect($DB_HOST,$DB_LOGIN,$DB_PASSWORD,$DB_NAME);
+
+
+// if (isset($links)) {echo "yes";} else echo "Noooooooooooooooo";
+if(!$link){
+    echo $DB_HOST;
+    
+	echo 'Ошибка: '
+		. mysqli_connect_errno()
+		. ':'
+		. mysqli_connect_error();
+}
